abde68acb912b1eb7b4f0bbdbd16758e8646ff54
bipidus513 <72986174+bipidus513@users.noreply.github.com>
2023-09-24 21:57:40 +0900

    Update packet.h

--- packet.h
+++ packet.h
@@ -10,20 +10,18 @@ struct ethheader {
 /* IP Header */
 #pragma pack(push,1)
 struct ipheader {
-  unsigned char      iph_ihl:4, //IP header length
-                     iph_ver:4; //IP version
-  unsigned char      iph_tos; //Type of service
-  unsigned short int iph_len; //IP Packet length (data + header)
-  unsigned short int iph_id; //Identification
-  unsigned short int iph_flag:3, //Fragmentation flags
-                     iph_offset:13; //Flags offset
-  unsigned char      iph_ttl; //Time to Live
-  unsigned char      iph_protocol; //Protocol type
-  unsigned short int iph_chksum; //IP datagram checksum
-  uint32_t           iph_sourceip; //Source IP address
-  uint32_t           iph_destip;   //Destination IP address
+  unsigned char ip_hl : 4;
+    unsigned char ip_v : 4; 
+    unsigned char ip_tos;
+    unsigned short ip_len;
+    unsigned short ip_id;
+    unsigned short ip_off;
+    unsigned char ip_ttl; 
+    unsigned char ip_p;  
+    unsigned short ip_sum;
+    BYTE ip_src[4]; 
+    BYTE ip_dst[4];  
 };
-
 
 /* ICMP Header  */
 struct icmpheader {
